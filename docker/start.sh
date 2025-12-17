4a20e2ae6d0753f597c0b9352511afb72c171cb1
Oleg Botnar <obotnar@dev.rtsoft.ru>
2025-12-17 15:46:27 +0300

    Drop dinasore name

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,12 @@
-DINASORE_DIR=/home/orangepi/dinasore/
+OPENFB_DIR=/home/orangepi/openfb/
 MODEL_FOLDER=/home/orangepi/demo
 
 
-docker run --rm  --privileged -v $DINASORE_DIR:/dinasore \
+docker run --rm  --privileged -v $OPENFB_DIR:/openfb \
     -v $MODEL_FOLDER:/demo \
     -v /proc/device-tree/compatible:/proc/device-tree/compatible \
     -v /dev/bus/usb:/dev/bus/usb \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -p 62499:62499 -p 4840:4840 \
     -e DISPLAY=:0.0 -e QT_X11_NO_MITSHM=1 \
-    -it dinasore:v0 
+    -it openfb:v0 
