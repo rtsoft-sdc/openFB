a3ddac22951a5a34b95f0394fdbd532b19aa0e2b
nfedorov <nfedorov@dev.rtsoft.ru>
2026-05-05 15:54:14 +0300

    remove not needed print

--- openfb/resources/function_blocks/convert/convert_basee.py
+++ openfb/resources/function_blocks/convert/convert_basee.py
@@ -1,6 +1,5 @@
 class ConvertBase:
     def schedule(self, event_name, event_value, IN):
         if event_name == 'REQ':
-            print(f"ConvertBase.schedule: {event_name}, {event_value}, {IN}")
             return event_value, IN
         return event_value, None
